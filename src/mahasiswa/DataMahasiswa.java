e0264c0cc2d349bdb73128b74d228b9017d39445
Ridho ilahi <riidho74@gmail.com>
2020-10-15 13:59:34 +0800

    +list data, konstruktor, adddata ke arraylist, fitur new, -adddata ke txt

---
+++ src/mahasiswa/DataMahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author ridho
+ */
+public class DataMahasiswa {
+    public String Name;
+    public String ID;
+    public String Smst;
+    public DataMahasiswa(String n, String id, String smst){
+        this.Name = n;
+        this.ID = id;
+        this.Smst = smst;
+    }
+}
